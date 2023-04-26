d92dae8e0efad1f21035c3af9052427a5706f795
oana.pintoiu <oana.pintoiu@yahoo.com>
2023-04-26 21:48:46 +0100

    complete

--- thermostat/thermostat.js
+++ thermostat/thermostat.js
@@ -1,16 +1,38 @@
 class Thermostat {
 constructor() {
     this.temperature = 20;
+    this.minTemp = 10;
+    this.maxTemp = 25;
 }
 
 getTemperature() {
-    return this.temperature
+    return this.temperature;
 }
 
 up(amount = 1) {
-this.temperature += amount; // increases temperature by 1
+    if (this.temperature < this.maxTemp) {
+return this.temperature += amount; // increases temperature by 1
+    }
 }
 
+down(amount = 1) {
+    if (this.temperature > this.minTemp) {
+    this.temperature -= amount; // decreses temperature by 1
+}
+}
+
+setPowerMode(mode) {
+    if (mode) {
+        this.maxTemp = 25;
+      } else {
+        this.maxTemp = 30;
+      }
+    }
+  
+
+reset() {
+    this.temperature = 20;
+};
 
 };
 
--- thermostat/thermostat.test.js
+++ thermostat/thermostat.test.js
@@ -1,18 +1,72 @@
-const Thermostat = require('./thermostat')
+const Thermostat = require("./thermostat");
 
-test('test to check there is a class Thermostat', () => {
-    const temp = new Thermostat();
+test("test to check there is a class Thermostat", () => {
+  const temp = new Thermostat();
 });
 
-test('class Thermostat returns temperature as 20', () => {
-const temp = new Thermostat();
-expect(temp.getTemperature()).toEqual(20);
+test("class Thermostat returns temperature as 20", () => {
+  const temp = new Thermostat();
+  expect(temp.getTemperature()).toEqual(20);
 });
 
-test('class Thermostat returns increased temperature', () => {
+test("class Thermostat returns increased temperature", () => {
+  const temp = new Thermostat();
+  temp.up();
+  temp.up();
+  expect(temp.getTemperature()).toEqual(22);
+});
+
+test("class Thermostat returns increased temperature", () => {
+  const temp = new Thermostat();
+  temp.up();
+  temp.up();
+  temp.down();
+  expect(temp.getTemperature()).toEqual(21);
+});
+
+test('Power saving mode is on', () => {
     const temp = new Thermostat();
+    for (let i = 0 ; i < 10 ; i++) {
+        temp.up();
+      }
+      expect(temp.getTemperature()).toBe(25);
+});
+
+test('Power saving mode is off', () => {
+  const temp = new Thermostat();
+  for (let i = 0 ; i < 10 ; i++) {
+      temp.up();
+    }
+    expect(temp.getTemperature()).toBe(25);
+    temp.setPowerMode(false);
+    temp.up();
+    expect(temp.getTemperature()).toBe(26);
+});
+
+test('Power saving mode is off', () => {
+  const temp = new Thermostat();
+  for (let i = 0 ; i < 10 ; i++) {
+      temp.up();
+    }
+    expect(temp.getTemperature()).toBe(25);
+    temp.setPowerMode(false);
     temp.up();
     temp.up();
-    expect(temp.getTemperature()).toEqual(22);
-    });
+    expect(temp.getTemperature()).toBe(27);
+});
+
+test('Power saving mode is off', () => {
+  const temp = new Thermostat();
+  for (let i = 0 ; i < 10 ; i++) {
+      temp.up();
+    }
+    expect(temp.getTemperature()).toBe(25);
+    temp.setPowerMode(false);
+    temp.up();
+    temp.up();
+    expect(temp.getTemperature()).toBe(27);
+    temp.reset();
+    expect(temp.getTemperature()).toBe(20);
+});
+
 
