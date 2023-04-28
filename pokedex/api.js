f8d84babf4d6384010de9979c2d942a4e32663be
oana.pintoiu <oana.pintoiu@yahoo.com>
2023-04-28 14:57:12 +0100

    extracted speciic information about pikachu

--- pokedex/api.js
+++ pokedex/api.js
@@ -3,7 +3,14 @@
 const fetchPokemon = (pokemon) => {
     return fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then((data) => console.log({
+        name: data.name,
+        id: data.id, 
+        height: data.height,
+        weight: data.weight,
+        types: data.types.map(item => item.type.name), // maps over data.types array and returns a new array with just the name of each type object
+        //this is an array.prototype.map() method used to transform each element of an array -> 'item' being the element of the array which is iterated over
+    }));
 };
 
 fetchPokemon('pikachu')
